2d3ee55c075f42b7d8771093dac187285670002c
Olivas312 <suporte.ti.rodrigo9@gmail.com>
2024-01-26 01:37:33 -0300

    Primeira etapa do desafio - Criptografia JS
    Concluído as etapas de HTML para desktop.
    Concluído a estilização do CSS.
    Concluído a etapa de criptografia em JS.
    Falta :
    Responsividade
    descriptografia JS
    Botao copiar

--- assets/js/arquivo.js
+++ assets/js/arquivo.js
@@ -1,7 +1,35 @@
-var btncripto = querySelector('#cripto')
+var btnCripto = document.querySelector('#cripto');
+var btnDescripto = document.querySelector('#descripto');
+function escondeEncripta(){
+    var textArea = document.querySelector('#decodificador');
+    var blockTxt404 = document.querySelector('#txt404');
+    var msg = document.querySelector('#msg');
+    var resposta = document.querySelector('#resposta');
 
-btncripto.addEventListerner('onclick',escondeEsconde())
+    textoOriginal = textArea.value;
+    textoCriptografado = [
+    textoOriginal.replace('e','enter')
+    .replace('i','imes')
+    .replace('a','ai')
+    .replace('o','ober')
+    .replace('u','ufat')];
 
-function escondeEsconde(){
-    alert('Estou funcionando')
-}
+    resposta.textContent = textoCriptografado;
+
+    // Adiciona um botão de cópia
+    resposta.innerHTML += '<button id="copiar" type="button" onclick="copiarTexto()">Copiar</button>';
+
+
+    if(msg.classList.contains('esconde')){
+        blockTxt404.classList.add('esconde');
+        msg.classList.remove('esconde')
+    }else{
+        blockTxt404.classList.remove('esconde');
+        msg.classList.add('esconde')
+    }
+
+};
+
+btnDescripto.addEventListener('click', escondeEncripta);
+btnCripto.addEventListener('click', escondeEncripta);
+
