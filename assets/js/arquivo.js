9495c18b11bbfc6c3ebd3fe36a0c5a1d3757a1f0
Olivas312 <suporte.ti.rodrigo9@gmail.com>
2024-01-27 00:27:43 -0300

    Primeira etapa do desafio - Criptografia JS
    Concluído as etapas de HTML para desktop.
    Concluído a estilização do CSS.
    Concluído a etapa de criptografia em JS.
    descriptografia JS
    Falta :
    Responsividade
    Botao copiar

--- assets/js/arquivo.js
+++ assets/js/arquivo.js
@@ -1,35 +1,72 @@
 var btnCripto = document.querySelector('#cripto');
 var btnDescripto = document.querySelector('#descripto');
-function escondeEncripta(){
-    var textArea = document.querySelector('#decodificador');
-    var blockTxt404 = document.querySelector('#txt404');
-    var msg = document.querySelector('#msg');
-    var resposta = document.querySelector('#resposta');
+var textArea = document.querySelector('#decodificador');
+var resposta = document.querySelector('#resposta');
 
-    textoOriginal = textArea.value;
-    textoCriptografado = [
+// função para encriptar o texto
+function encripta(){
+
+   var textoOriginal = textArea.value;
+   var textoCriptografado = 
     textoOriginal.replace('e','enter')
     .replace('i','imes')
     .replace('a','ai')
     .replace('o','ober')
-    .replace('u','ufat')];
-
+    .replace('u','ufat');        
+        // escreve no HTML
     resposta.textContent = textoCriptografado;
+    
+    var botaoCopiar = document.createElement('button');
+    botaoCopiar.id = 'copiar';
+    botaoCopiar.type = 'button';
+    botaoCopiar.textContent = 'Copiar';
+    botaoCopiar.addEventListener('click', copiarTexto);
+    resposta.appendChild(botaoCopiar);
+};
+// função para reverter a criptografia
+function desencriptar(){
+    
+    var textoOriginal = textArea.value;
+    var textoCriptografado = 
+    textoOriginal.replace('enter','e')
+    .replace('imes','i')
+    .replace('ai'  ,'a')
+    .replace('ober','o')
+    .replace('ufat','u');
+        // escreve no HTML
+    resposta.textContent = textoCriptografado;
+    
+    var botaoCopiar = document.createElement('button');
+    botaoCopiar.id = 'copiar';
+    botaoCopiar.type = 'button';
+    botaoCopiar.textContent = 'Copiar';
+    botaoCopiar.addEventListener('click', copiarTexto);
+    resposta.appendChild(botaoCopiar);
 
-    // Adiciona um botão de cópia
-    resposta.innerHTML += '<button id="copiar" type="button" onclick="copiarTexto()">Copiar</button>';
-
-
-    if(msg.classList.contains('esconde')){
+}
+//  Função para esconder a imagem de texto não encontrado e revelar a resposta
+function esconde(){
+    var blockTxt404 = document.querySelector('#txt404');
+    var msg = document.querySelector('#msg');
+    if(textArea.value.length > 0){
+        if(msg.classList.contains('esconde')){
         blockTxt404.classList.add('esconde');
         msg.classList.remove('esconde')
-    }else{
+        }else{
         blockTxt404.classList.remove('esconde');
         msg.classList.add('esconde')
-    }
-
+        }
+    }else{
+        alert("Insira sua mensagem !")
+    };
 };
 
-btnDescripto.addEventListener('click', escondeEncripta);
-btnCripto.addEventListener('click', escondeEncripta);
+btnDescripto.addEventListener('click', function(){
+    esconde();
+    desencriptar();
+});
+btnCripto.addEventListener('click', function(){
+    esconde();
+    encripta();
+});
 
